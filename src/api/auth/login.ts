edec024f6837612924c2abe94da7cedcdcf59f55
나승엽 <a01031928263@gmail.com>
2024-11-16 07:34:47 +0900

    refactor: Update login functionality

--- src/api/auth/login.ts
+++ src/api/auth/login.ts
@@ -1,18 +1,15 @@
 import axios from 'axios';
 import { AUTH_URL } from '@/constants/urls';
 
-export default function handleLogin(email: string, password: string) {
-  return axios
-    .post(`${AUTH_URL}/login`, { email, password })
-    .then(function (response) {
-      const { accessToken } = response.data;
+export default async function handleLogin(email: string, password: string) {
+  try {
+    const response = await axios.post(`${AUTH_URL}/login`, { email, password });
+    const { accessToken } = response.data;
 
-      localStorage.setItem('accessToken', accessToken);
-
-      return true;
-    })
-    .catch(function (error) {
-      console.error('로그인 실패:', error);
-      return false;
-    });
+    localStorage.setItem('accessToken', accessToken);
+    return true;
+  } catch (error) {
+    console.error('로그인 실패:', error);
+    return false;
+  }
 }
