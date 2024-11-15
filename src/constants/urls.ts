b69727b2c76bcca3018345b7b68857745ec60dfe
najitwo <jitwona99@gmail.com>
2024-11-15 11:46:20 +0900

    🗃️ chore: add constants for API URL

---
+++ src/constants/urls.ts
@@ -0,0 +1,10 @@
+const BASE_URL = 'https://sp-taskify-api.vercel.app/10-1';
+
+export const AUTH_URL = `${BASE_URL}/auth`;
+export const CARD_URL = `${BASE_URL}/cards`;
+export const COLUMN_URL = `${BASE_URL}/columns`;
+export const COMMENT_URL = `${BASE_URL}/comments`;
+export const DASHBOARD_URL = `${BASE_URL}/dashboards`;
+export const INVITATION_URL = `${BASE_URL}/invitations`;
+export const MEMBER_URL = `${BASE_URL}/members`;
+export const USER_URL = `${BASE_URL}/users`;
