ecd4a4c0655ef1efc4e806fba5b1c79dcd376ac3
21ow <kimjinewn@gmail.com>
2024-11-16 00:54:52 +0900

    :sparkles: feat: add logo to header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
+'use client';
+
 import styles from './page.module.css';
+import dynamic from 'next/dynamic';
+
+const Logo = dynamic(() => import('../../public/images/logo_large.svg'), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <header>
-        <div></div>
+        <div>
+          <Logo aria-hidden="true" />
+        </div>
         <nav></nav>
       </header>
 
