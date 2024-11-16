c4311a76b4ecc07569438402aeae1d94430d1a2d
heejin <dlwlrgkwk82@gmail.com>
2024-11-16 20:35:24 +0900

    💄 style: add header button icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
 'use client';
 import { usePathname } from 'next/navigation';
 import type { User } from '@/app/(with-header-sidebar)/mydashboard/types/user';
+import Image from 'next/image';
 import Button from './Button';
+import useWindowSize from '@/app/(with-header-sidebar)/mydashboard/hooks/useWindowSize';
 import styles from './Header.module.css';
 
 const user: User = {
@@ -18,6 +20,7 @@ interface HeaderProps {
 
 export default function Header({ component: Component }: HeaderProps) {
   const pathname = usePathname();
+  const { isMobile } = useWindowSize();
 
   const { nickname, profileImageUrl } = user;
 
@@ -25,8 +28,28 @@ export default function Header({ component: Component }: HeaderProps) {
     <header className={styles.header}>
       <span className={styles.title}>나의 대시보드</span>
       <div className={styles.buttonContainer}>
-        <Button className={styles.button}>관리</Button>
-        <Button className={styles.button}>초대하기</Button>
+        <Button className={styles.button}>
+          {!isMobile && (
+            <Image
+              src="/icons/settings.svg"
+              alt="관리"
+              width={20}
+              height={20}
+            />
+          )}
+          관리
+        </Button>
+        <Button className={styles.button}>
+          {!isMobile && (
+            <Image
+              src="/icons/add_box.svg"
+              alt="초대하기"
+              width={20}
+              height={20}
+            />
+          )}
+          초대하기
+        </Button>
       </div>
       {Component && (
         <div>
