ab405fc00e0472ecc605d1a8fe174db3c448c350
heejin <dlwlrgkwk82@gmail.com>
2024-11-16 22:39:54 +0900

    ✨ feat: add userInfo component with skeleton

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,9 @@
 'use client';
 import { usePathname } from 'next/navigation';
-import type { User } from '@/app/(with-header-sidebar)/mydashboard/types/user';
 import Image from 'next/image';
 import Button from './Button';
-import useWindowSize from '@/app/(with-header-sidebar)/mydashboard/hooks/useWindowSize';
+import UserInfo from './UserInfo';
 import styles from './Header.module.css';
-
-const user: User = {
-  id: 1,
-  email: 'heejin@gmail.com',
-  nickname: 'heejin',
-  profileImageUrl: null,
-  createdAt: '2024-11-15T14:29:07.482Z',
-};
 
 interface HeaderProps {
   component: React.ComponentType;
@@ -20,34 +11,29 @@ interface HeaderProps {
 
 export default function Header({ component: Component }: HeaderProps) {
   const pathname = usePathname();
-  const { isMobile } = useWindowSize();
-
-  const { nickname, profileImageUrl } = user;
 
   return (
     <header className={styles.header}>
-      <span className={styles.title}>나의 대시보드</span>
+      <span className={styles.title}>내 대시보드</span>
       <div className={styles.buttonContainer}>
         <Button className={styles.button}>
-          {!isMobile && (
-            <Image
-              src="/icons/settings.svg"
-              alt="관리"
-              width={20}
-              height={20}
-            />
-          )}
+          <Image
+            src="/icons/settings.svg"
+            alt="관리"
+            width={20}
+            height={20}
+            className={styles.icon}
+          />
           관리
         </Button>
         <Button className={styles.button}>
-          {!isMobile && (
-            <Image
-              src="/icons/add_box.svg"
-              alt="초대하기"
-              width={20}
-              height={20}
-            />
-          )}
+          <Image
+            src="/icons/add_box.svg"
+            alt="초대하기"
+            width={20}
+            height={20}
+            className={styles.icon}
+          />
           초대하기
         </Button>
       </div>
@@ -56,7 +42,11 @@ export default function Header({ component: Component }: HeaderProps) {
           <Component />
         </div>
       )}
-      <div>userInfo</div>
+      <div className={styles.userInfoContainer}>
+        <div className={styles.userInfoWrapper}>
+          <UserInfo />
+        </div>
+      </div>
     </header>
   );
 }
