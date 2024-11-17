05f682b10f97552b996d269c5ba924f09c5068cd
heejin <dlwlrgkwk82@gmail.com>
2024-11-17 17:11:22 +0900

    💄 style: add sidebar dashboard style

--- src/components/sidebar/Dashboards.tsx
+++ src/components/sidebar/Dashboards.tsx
@@ -1,34 +1,51 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import type { GetDashboardsResponse } from '@/app/(with-header-sidebar)/mydashboard/_types/dashboards';
+import type {
+  Dashboard,
+  GetDashboardsResponse,
+} from '@/app/(with-header-sidebar)/mydashboard/_types/dashboards';
 import styles from './Dashboards.module.css';
 import useApi from '@/app/(with-header-sidebar)/mydashboard/_hooks/useApi';
+import Image from 'next/image';
 
 export default function Dashboards() {
-  const pathname = usePathname();
-
   const { data } = useApi<GetDashboardsResponse>('/dashboards', {
     method: 'GET',
     params: { navigationMethod: 'infiniteScroll', page: 1, size: 10 },
   });
 
-  const dashboards = data?.dashboards;
+  const dashboards = data?.dashboards ?? [];
 
   return (
     <div className={styles.dashBoards}>
       <ul>
-        {dashboards &&
-          dashboards.map((board) => (
-            <li key={board.id}>
-              <Link
-                href={`/dashboard/${board.id}`}
-                className={`link ${pathname === '/dashboard/' + board.id ? styles.active : ''}`}
-              >
-                {board.title}
-              </Link>
-            </li>
-          ))}
+        {dashboards.map((board) => (
+          <DashboardItem key={board.id} {...board} />
+        ))}
       </ul>
     </div>
+  );
+}
+
+function DashboardItem({ id, color, title, createdByMe }: Dashboard) {
+  const isActive = usePathname() === `/dashboard/${id}`;
+
+  return (
+    <li>
+      <Link
+        href={`/dashboard/${id}`}
+        className={`link ${isActive ? styles.active : ''}`}
+      >
+        <div className={styles.titleContainer}>
+          <div style={{ background: color }} className={styles.dot}></div>
+          <span className={styles.title}>{title}</span>
+          {createdByMe && (
+            <span className={styles.crown}>
+              <Image src="/icons/crown.svg" alt="왕관" width={15} height={12} />
+            </span>
+          )}
+        </div>
+      </Link>
+    </li>
   );
 }
