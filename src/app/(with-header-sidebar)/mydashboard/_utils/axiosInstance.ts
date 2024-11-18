0a2a5a7d20c8cad0dc0d985d52e3e47176ed7084
heejin <77238424+devmanta@users.noreply.github.com>
2024-11-18 11:32:52 +0900

    [feat] add sidebar dashboards (#18)
    * 💄 style: sidebar dashboard active style
    
    * 💄 style: add sidebar dashboard style
    
    * 💄 style: add sidebar dashboard gap and title style
    
    * 💄 style: modify sidebar border
    
    * 🩹 style: roll back for sidebar border
    
    * ✨ feat: add sidebar dashboard pagination
    
    * 💄 style: add sidebar pagniation arrow css
    
    * ✨ feat: add navigation on logo
    
    * ✨ feat: add header myInfo menu
    
    * ♻️ refactor: extract user type
    
    * ♻️ refactor: remove DashboardList function

--- src/app/(with-header-sidebar)/mydashboard/_utils/axiosInstance.ts
+++ src/app/(with-header-sidebar)/mydashboard/_utils/axiosInstance.ts
@@ -13,6 +13,7 @@ axiosInstance.interceptors.request.use(
   (config) => {
     const token =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NDgwNCwidGVhbUlkIjoiMTAtMSIsImlhdCI6MTczMTcyMzkwNywiaXNzIjoic3AtdGFza2lmeSJ9.k8FqEAl7DbhwxhJNAkkMq8lYrgStN-9I3xrsR0cYm2c'; // TODO: Add token
+    // 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NDgwNywidGVhbUlkIjoiMTAtMSIsImlhdCI6MTczMTc2OTIwNywiaXNzIjoic3AtdGFza2lmeSJ9.yhISPAxnBlD28SkCY0mUxcIM5YuwAAib2k7j15fmlvA'; // TODO: Add token
     if (token) {
       config.headers['Authorization'] = `Bearer ${token}`;
     }
