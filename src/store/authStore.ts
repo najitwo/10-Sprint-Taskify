3abcc124b6ab3d08dc8b770c5777e5bc791ca601
나승엽 <a01031928263@gmail.com>
2024-11-18 12:06:41 +0900

    :recycle: refactor: authStore for token handling

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -8,41 +8,11 @@ interface AuthState {
   setUser: (user: User | null) => void;
 }
 
-<<<<<<< HEAD
 const useAuthStore = create<AuthState>((set) => ({
   user: null,
   accessToken: null,
   setUser: (user) => set({ user }),
   setAccessToken: (accessToken) => set({ accessToken }),
 }));
-=======
-type SetState = <T>(fn: (state: T) => T) => void;
-
-function setAccessTokenFn(set: SetState, token: string) {
-  set(function (state: AuthState) {
-    return { ...state, accessToken: token };
-  });
-}
-
-function removeAccessTokenFn(set: SetState) {
-  set(function (state: AuthState) {
-    return { ...state, accessToken: null };
-  });
-}
-
-function createAuthStore(set: SetState) {
-  return {
-    accessToken: null,
-    setAccessToken: function (token: string) {
-      setAccessTokenFn(set, token);
-    },
-    removeAccessToken: function () {
-      removeAccessTokenFn(set);
-    },
-  };
-}
-
-const useAuthStore = create<AuthState>(createAuthStore);
->>>>>>> 4371164 (:bug: refactor: refactor update types in zustand store)
 
 export default useAuthStore;
