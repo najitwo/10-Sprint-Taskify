9f64b3261ee41d58455a609458cf456721d8f049
heejin <dlwlrgkwk82@gmail.com>
2024-11-18 15:08:58 +0900

    ✨ feat: add mydashboard add dashboard button

--- src/app/(with-header-sidebar)/mydashboard/_components/dashboards/Dashboards.tsx
+++ src/app/(with-header-sidebar)/mydashboard/_components/dashboards/Dashboards.tsx
@@ -4,6 +4,8 @@ import useDashboards from '../../_hooks/useDashboards';
 import Pagination from './Pagination';
 import styles from './Dashboards.module.css';
 import DashboardCard from './DashboardCard';
+import Button from '../Button';
+import Image from 'next/image';
 
 const PAGE_SIZE = 5;
 
@@ -15,6 +17,17 @@ export default function Dashboards() {
   return (
     <div>
       <section className={styles.dashboardsWrapper}>
+        <Button className={styles.addDashboard}>
+          <span>새로운 대시보드</span>
+          <span className={styles.addIconWrapper}>
+            <Image
+              src="/icons/add.svg"
+              alt="새로운 대시보드 추가"
+              width={16}
+              height={16}
+            />
+          </span>
+        </Button>
         {dashboards.map((board) => (
           <DashboardCard key={board.id} {...board} />
         ))}
