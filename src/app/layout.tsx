e7eacee224e40e34f999291511e8e707661dc7ec
najitwo <jitwona99@gmail.com>
2024-11-18 20:11:27 +0900

    🐛 fix: correct typo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="kr">
+    <html lang="ko">
       <body className={`${pretendard.variable} ${montserrat.variable}`}>
         {children}
       </body>
