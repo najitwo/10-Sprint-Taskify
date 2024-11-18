31e54f916e33f8a8044511703ab81380195612e1
나승엽 <a01031928263@gmail.com>
2024-11-18 12:06:41 +0900

    :recycle: refactor: authStore for token handling

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,37 +1,18 @@
 import { create } from 'zustand';
+import { User } from '../types/user';
 
 interface AuthState {
   accessToken: string | null;
-  setAccessToken: (token: string) => void;
-  removeAccessToken: () => void;
+  user: User | null;
+  setAccessToken: (accessToken: string | null) => void;
+  setUser: (user: User | null) => void;
 }
 
-type SetState = <T>(fn: (state: T) => T) => void;
-
-function setAccessTokenFn(set: SetState, token: string) {
-  set(function (state: AuthState) {
-    return { ...state, accessToken: token };
-  });
-}
-
-function removeAccessTokenFn(set: SetState) {
-  set(function (state: AuthState) {
-    return { ...state, accessToken: null };
-  });
-}
-
-function createAuthStore(set: SetState) {
-  return {
-    accessToken: null,
-    setAccessToken: function (token: string) {
-      setAccessTokenFn(set, token);
-    },
-    removeAccessToken: function () {
-      removeAccessTokenFn(set);
-    },
-  };
-}
-
-const useAuthStore = create<AuthState>(createAuthStore);
+const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  accessToken: null,
+  setUser: (user) => set({ user }),
+  setAccessToken: (accessToken) => set({ accessToken }),
+}));
 
 export default useAuthStore;
