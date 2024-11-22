dbd1c7e4507b562c61f87ff78cae1134b0d20f59
heejin <dlwlrgkwk82@gmail.com>
2024-11-22 17:17:15 +0900

    🎨 style: add empty myInvitation

--- src/app/(with-header-sidebar)/mydashboard/_components/invitations/MyInvitations.tsx
+++ src/app/(with-header-sidebar)/mydashboard/_components/invitations/MyInvitations.tsx
@@ -3,40 +3,41 @@
 import Image from 'next/image';
 import styles from './MyInvitations.module.css';
 import { useMyInvitations } from '../../_hooks/useMyInvitations';
-import Button from '@/components/Button';
 import MyInvitationCard from './MyInvitationCard';
 
 export default function MyInvitations() {
   const { myInvitations, isLoading, error, observerRef } = useMyInvitations();
 
+  if (error) {
+    return <div>공습경보🚨</div>;
+  }
+
   return (
     <section className={styles.invitations}>
       <h2 className={styles.title}>초대받은 대시보드</h2>
-      {/* <div className={styles.descriptionWrapper}>
-        <Image
-          src="/images/unsubscribe.svg"
-          alt="메일없음 이미지"
-          width={100}
-          height={100}
-        />
-        <p className={styles.description}>아직 초대받은 대시보드가 없어요</p>
-      </div> */}
-      <div>
-        <div>searchbar</div>
-        <div className={styles.invitationWrapper}>
-          <div className={styles.header}>
-            <span>이름</span>
-            <span>초대자</span>
-            <span>수락여부</span>
+      {myInvitations.length > 0 ? (
+        <div>
+          <div>searchbar</div>
+          <div className={styles.invitationWrapper}>
+            {myInvitations.map((invitation) => (
+              <div key={invitation.id} className={styles.myInvitation}>
+                <MyInvitationCard {...invitation} />
+              </div>
+            ))}
           </div>
-          {myInvitations.map((invitation) => (
-            <div key={invitation.id} className={styles.myInvitation}>
-              <MyInvitationCard {...invitation} />
-            </div>
-          ))}
+          <div ref={observerRef} style={{ height: '1px' }}></div>
         </div>
-        <div ref={observerRef} style={{ height: '1px' }}></div>
-      </div>
+      ) : (
+        <div className={styles.descriptionWrapper}>
+          <Image
+            src="/images/unsubscribe.svg"
+            alt="메일없음 이미지"
+            width={100}
+            height={100}
+          />
+          <p className={styles.description}>아직 초대받은 대시보드가 없어요</p>
+        </div>
+      )}
     </section>
   );
 }
