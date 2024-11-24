c3bd81cf23688eee35731330560a20ebfb4d35cc
naji <jitwona99@gmail.com>
2024-11-24 22:32:25 +0900

    [feat] add dashboard deletion (#65)
    * ✨ feat: add dashboard deletion
    
    * ♻️ refactor: improve ListItem component
    
    Updated Avatar component to displaying user image.
    Added crown icon for dashboard owner.
    
    * 🚚 refactor: move shared functions to common utilities folder

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,25 +1,49 @@
+import Image from 'next/image';
 import styles from './Avatar.module.css';
 
-function getRandomColor(name: string): string {
-  const hash = [...name].reduce(
-    (acc, char) => acc + char.charCodeAt(0) * 31,
-    0
-  );
-  const getValue = (offset: number) => (((hash >> offset) % 0xff) % 76) + 180;
-  const red = getValue(0);
-  const green = getValue(8);
-  const blue = getValue(16);
-
-  return `rgb(${red}, ${green}, ${blue})`;
+interface AvatarProps {
+  name: string;
+  profileImageUrl?: string | null;
+  className?: string;
 }
 
-export default function Avatar({ name }: { name: string }) {
+export default function Avatar({
+  name,
+  profileImageUrl = null,
+  className = '',
+}: AvatarProps) {
+  const getRandomColor = (name: string) => {
+    const hash = [...name].reduce(
+      (acc, char) => acc + char.charCodeAt(0) * 31,
+      0
+    );
+    const getValue = (offset: number) => (((hash >> offset) % 0xff) % 76) + 180;
+    const red = getValue(0);
+    const green = getValue(8);
+    const blue = getValue(16);
+
+    return `rgb(${red}, ${green}, ${blue})`;
+  };
+
   return (
-    <div
-      className={styles.avatar}
-      style={{ backgroundColor: `${getRandomColor(name)}` }}
-    >
-      {name[0].toUpperCase()}
-    </div>
+    <>
+      {profileImageUrl ? (
+        <div className={`${styles.avatar} ${className}`}>
+          <Image
+            src={profileImageUrl}
+            alt="프로필 이미지"
+            fill
+            className={styles.image}
+          />
+        </div>
+      ) : (
+        <div
+          className={`${styles.avatar} ${className}`}
+          style={{ backgroundColor: `${getRandomColor(name)}` }}
+        >
+          {name[0].toUpperCase()}
+        </div>
+      )}
+    </>
   );
 }
