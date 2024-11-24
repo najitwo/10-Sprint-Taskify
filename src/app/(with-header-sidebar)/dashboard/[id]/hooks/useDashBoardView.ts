8b3a94f7f6dfdfc663e2e4727cf4893650445e4e
21ow <kimjinewn@gmail.com>
2024-11-24 15:59:23 +0900

    Merge branch '48-refactor-split-components-into-separate-files'

---
+++ src/app/(with-header-sidebar)/dashboard/[id]/hooks/useDashBoardView.ts
@@ -0,0 +1,149 @@
+import { useState, useEffect, useCallback } from 'react';
+import debounce from 'lodash/debounce';
+import axiosInstance from '@/lib/axiosInstance';
+import { DropResult } from 'react-beautiful-dnd';
+import { ColumnData, CardData } from '@/types/dashboardView';
+import { COLUMN_URL, CARD_URL } from '@/constants/urls';
+
+export default function useDashBoardView(dashboardId: string | undefined) {
+  const [columns, setColumns] = useState<ColumnData[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const [cursor, setCursor] = useState<string | null>(null);
+
+  const fetchData = useCallback(async () => {
+    setLoading(true);
+    try {
+      const { data: columnData } = await axiosInstance.get(
+        `${COLUMN_URL}?dashboardId=${dashboardId}`
+      );
+
+      const columns = columnData.data;
+
+      const columnIds: number[] = columns.map(
+        (column: ColumnData) => column.id
+      );
+
+      const cardRequests = columnIds.map((columnId) =>
+        axiosInstance.get(
+          `${CARD_URL}?size=10&columnId=${columnId}&cursor=${cursor || ''}`
+        )
+      );
+
+      const cardResponses = await Promise.all(cardRequests);
+      console.log(cardResponses[0].data.totalCount);
+
+      const updatedColumns = columns.map(
+        (column: ColumnData, index: number) => {
+          const cardData = cardResponses[index].data.cards;
+          const totalCount = cardResponses[index].data.totalCount;
+          return {
+            ...column,
+            items: cardData || [],
+            totalCount: totalCount || 0,
+          };
+        }
+      );
+      setColumns(updatedColumns);
+      setCursor(cardResponses[0].data.cursor);
+    } catch (err: unknown) {
+      if (err instanceof Error) setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  }, [dashboardId, cursor]);
+  //체크
+
+  useEffect(() => {
+    console.log('렌더링 체크');
+    if (!dashboardId) return;
+
+    fetchData();
+  }, [fetchData, dashboardId, cursor]);
+  //체크
+
+  const sendCardUpdateRequest = useCallback(
+    debounce(async (cardId: string, updatedCardData: CardData) => {
+      try {
+        const response = await axiosInstance.put(
+          `${CARD_URL}/${cardId}`,
+          updatedCardData
+        );
+        console.log('업뎃카드데이터', updatedCardData);
+        if (response.status === 200 || response.status === 204) {
+          console.log('카드가 성공적으로 업데이트되었습니다.');
+        }
+      } catch (err: unknown) {
+        if (err instanceof Error) setError(err.message);
+      }
+    }, 200),
+    []
+  );
+  //체크
+
+  const handleOnDragEnd = useCallback(
+    async (result: DropResult) => {
+      const { source, destination } = result;
+
+      if (!destination) return;
+
+      const sourceDroppableId = Number(source.droppableId);
+      const destinationDroppableId = Number(destination.droppableId);
+
+      let removed;
+      const updatedColumns = [...columns];
+
+      if (sourceDroppableId === destinationDroppableId) {
+        const columnIndex = updatedColumns.findIndex(
+          (col) => col.id === sourceDroppableId
+        );
+        const column = updatedColumns[columnIndex];
+        const reorderedItems = Array.from(column.items);
+        [removed] = reorderedItems.splice(source.index, 1);
+        reorderedItems.splice(destination.index, 0, removed);
+        updatedColumns[columnIndex].items = reorderedItems;
+      } else {
+        const sourceColumnIndex = updatedColumns.findIndex(
+          (col) => col.id === sourceDroppableId
+        );
+        const destinationColumnIndex = updatedColumns.findIndex(
+          (col) => col.id === destinationDroppableId
+        );
+
+        const sourceColumn = updatedColumns[sourceColumnIndex];
+        const destinationColumn = updatedColumns[destinationColumnIndex];
+
+        const sourceItems = Array.from(sourceColumn.items);
+        [removed] = sourceItems.splice(source.index, 1);
+        removed.columnId = destinationColumn.id;
+
+        const destinationItems = Array.from(destinationColumn.items);
+        destinationItems.splice(destination.index, 0, removed);
+
+        updatedColumns[sourceColumnIndex].items = sourceItems;
+        updatedColumns[destinationColumnIndex].items = destinationItems;
+
+        updatedColumns[sourceColumnIndex].totalCount -= 1;
+        updatedColumns[destinationColumnIndex].totalCount += 1;
+
+        console.log(updatedColumns[sourceColumnIndex].totalCount);
+      }
+
+      setColumns(updatedColumns);
+
+      try {
+        await sendCardUpdateRequest(`${removed.id}`, {
+          ...removed,
+          columnId: removed.columnId,
+        });
+      } catch (err: unknown) {
+        if (err instanceof Error) setError(err.message);
+      }
+    },
+    [columns, sendCardUpdateRequest]
+  );
+
+  //체크
+
+  return { columns, loading, error, handleOnDragEnd };
+}
---
+++ src/types/dashboardView.ts
@@ -0,0 +1,33 @@
+export interface Assignee {
+  profileImageUrl: string;
+  nickname: string;
+  id: number;
+}
+
+export interface CardData {
+  id: number;
+  title: string;
+  description: string;
+  tags: string[];
+  dueDate: string;
+  assignee: Assignee;
+  imageUrl: string;
+  teamId: string;
+  columnId: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface CardResponse {
+  cursorId: number;
+  totalCount: number;
+  cards: CardData[];
+}
+
+export interface ColumnData {
+  title: string;
+  color: string;
+  totalCount: number;
+  id: number;
+  items: CardData[];
+}
