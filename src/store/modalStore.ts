8b1119852fae466226f55c72bf74ae71070e20cd
najitwo <jitwona99@gmail.com>
2024-11-25 19:29:36 +0900

    ✨ feat: add animation and FocusTrap to modal

--- src/store/modalStore.ts
+++ src/store/modalStore.ts
@@ -3,19 +3,32 @@ import { ReactNode } from 'react';
 
 interface ModalState {
   modals: ReactNode[];
+  isModalVisible: boolean;
   openModal: (content: ReactNode) => void;
   closeModal: () => void;
+  closeAllModal: () => void;
 }
 
 const useModalStore = create<ModalState>((set) => ({
   modals: [],
+  isModalVisible: false,
   openModal: (content) =>
     set((state) => ({
       modals: [...state.modals, content],
+      isModalVisible: true,
     })),
-  closeModal: () =>
-    set((state) => ({
-      modals: state.modals.slice(0, -1),
+  closeModal: () => {
+    set({ isModalVisible: false });
+    setTimeout(() => {
+      set((state) => ({
+        modals: state.modals.slice(0, -1),
+      }));
+    }, 300);
+  },
+  closeAllModal: () =>
+    set(() => ({
+      modals: [],
+      isModalVisible: false,
     })),
 }));
 
