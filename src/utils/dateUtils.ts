a1634ccacc3ed5a6c8f8c94f8fdc2c5afdf1d819
najitwo <jitwona99@gmail.com>
2024-11-27 02:07:20 +0900

    💄 style: improve DatePicker

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -11,3 +11,15 @@ export function formatDateToCustomFormat(targetDate: string): string {
 
   return `${year}.${month}.${day} ${hours}:${minutes}`;
 }
+
+export function formatDateFormat(targetDate: string | Date): string {
+  if (!targetDate) return '';
+
+  const date = new Date(targetDate);
+
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+
+  return `${year}. ${month}. ${day}`;
+}
