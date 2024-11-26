7a39b3ae362218e5d71b3a7eb16f2984fd0166a2
나승엽 <166021800+naseungyeop@users.noreply.github.com>
2024-11-27 01:44:52 +0900

    [feat] add auth layout (#61)
    * :sparkles: feat: add zustand store and fix login functionality
    
    * :bug: fix: linting issues
    
    * :bug: refactor: refactor update types in zustand store
    
    * :bug: refactor: update types in zustand store2
    
    * :recycle: refactor: authStore for token handling
    
    * :sparkles: feat: add login page
    
    * :sparkles: feat: add login page
    
    * :fire: remove: styled components
    
    * :fix: chore: restore package-lock.json
    
    * :sparkles: feat: add authlayout component and implement responsive design
    
    * :recycle: refactor: modify authlayout structure
    
    * :recycle: refactor: rename layout file update login page structure and modify package-lock.json

---
+++ src/app/(user-access)/layout.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import React from 'react';
+import styles from './layout.module.css';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+interface AuthLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function AuthLayout({ children }: AuthLayoutProps) {
+  const router = useRouter();
+
+  return (
+    <div className={styles.authContainer}>
+      <div className={styles.logoContainer}>
+        <Image
+          src="/images/logo_main.svg"
+          alt="로고"
+          width={100}
+          height={100}
+          className={styles.logo}
+          onClick={() => router.push('/')}
+          style={{ cursor: 'pointer' }}
+        />
+      </div>
+      <div className={styles.authContent}>{children}</div>
+    </div>
+  );
+}
