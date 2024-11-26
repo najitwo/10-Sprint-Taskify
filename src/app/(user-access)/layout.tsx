d5a393caedb35266d734509f5a2ed7a78c26d816
나승엽 <a01031928263@gmail.com>
2024-11-26 13:53:16 +0900

    :recycle: refactor: rename layout file update login page structure and modify package-lock.json

--- src/app/(user-access)/layout.tsx
+++ src/app/(user-access)/layout.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import styles from './AuthLayout.module.css';
+import styles from './layout.module.css';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
