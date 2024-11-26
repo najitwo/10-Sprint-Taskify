3e9843c449c49610ab3b61d2339b1a03e3d78d34
heejin <dlwlrgkwk82@gmail.com>
2024-11-26 17:45:11 +0900

    🔥 remove: remove sample data

--- src/app/(with-header-sidebar)/dashboard/[id]/components/card-detail/CardInfo.tsx
+++ src/app/(with-header-sidebar)/dashboard/[id]/components/card-detail/CardInfo.tsx
@@ -6,35 +6,14 @@ import Pipe from '@/components/svg/Pipe';
 import Image from 'next/image';
 import styles from './CardInfo.module.css';
 
-const sample = {
-  id: 1,
-  title: '카드타이틀',
-  description:
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex molestias enim beatae in accusantium cumque necessitatibus quia, dolorem laboriosam soluta maxime error minima. Dolores quis culpa aut hic, cum dolorem?',
-  tags: ['마늘', '기여어', '세젤귀', '맨날자'],
-  dueDate: '2024-11-11 11:00',
-  assignee: {
-    profileImageUrl: null,
-    nickname: '김희진',
-    id: 1,
-  },
-  imageUrl:
-    'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/taskify/profile_image/10-1_4804_1731757528194.jpeg',
-  // null,
-  teamId: '1',
-  columnId: 1234,
-  createdAt: '2024',
-  updatedAt: '2024',
-};
-
 interface CardInfoProps {
   card: CardData;
   columnTitle: string;
 }
 
 export default function CardInfo({ card, columnTitle }: CardInfoProps) {
-  card = sample;
   const { description, tags, imageUrl } = card;
+
   return (
     <div className={styles.cardInfo}>
       <div className={styles.assignmentContainer}>
