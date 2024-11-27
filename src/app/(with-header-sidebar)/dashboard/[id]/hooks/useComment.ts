633e683712ba985e1fa6ef41253059b9b3f4d7ce
najitwo <jitwona99@gmail.com>
2024-11-27 14:33:48 +0900

    ✨ feat: implement comments fetch

---
+++ src/app/(with-header-sidebar)/dashboard/[id]/hooks/useComment.ts
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react';
+import { useIntersectionObserver } from '@/app/(with-header-sidebar)/mydashboard/_hooks/useIntersectionObserver';
+import { Comment } from '@/types/comment';
+import { getComments } from '@/lib/commentService';
+
+const useComment = (cardId: number, pageSize: number = 3) => {
+  const [comments, setComments] = useState<Comment[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [cursorId, setCursorId] = useState<number | null>(null);
+
+  const fetchComments = async () => {
+    if (isLoading) return;
+
+    setIsLoading(true);
+    setError(null);
+
+    const params: { cardId: number; size: number; cursorId?: number } = {
+      cardId,
+      size: pageSize,
+      ...(cursorId !== null && { cursorId }),
+    };
+
+    try {
+      getComments;
+      const response = await getComments(params);
+      setComments((prev) => [...prev, ...response.comments]);
+      setCursorId(response.cursorId || null);
+    } catch (err) {
+      console.error(err);
+      setError('Failed to fetch invitations');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    setComments([]);
+    setCursorId(null);
+    fetchComments();
+  }, []);
+
+  const observerRef = useIntersectionObserver(
+    fetchComments,
+    isLoading,
+    cursorId
+  );
+
+  return {
+    comments,
+    isLoading,
+    error,
+    observerRef,
+  };
+};
+
+export default useComment;
