7fd167db4b02bfee7914ce0b9263c47388a4ceaf
heejin <dlwlrgkwk82@gmail.com>
2024-11-27 17:34:57 +0900

    💄 style: card preview css

--- src/app/(with-header-sidebar)/dashboard/[id]/components/Card.tsx
+++ src/app/(with-header-sidebar)/dashboard/[id]/components/Card.tsx
@@ -6,6 +6,9 @@ import Modal from '@/app/(with-header-sidebar)/mydashboard/_components/modal/Mod
 import CardInfo from './card-detail/CardInfo';
 import HeaderMenu from './card-detail/HeaderMenu';
 import Image from 'next/image';
+import Tag from '@/components/card/Tag';
+import CalendarIcon from '/public/icons/calendar.svg';
+import Avatar from '@/components/Avatar';
 import styles from './Card.module.css';
 
 interface Props {
@@ -21,7 +24,7 @@ function Card({ item, index, columnTitle }: Props) {
     return null;
   }
 
-  const { id, title, imageUrl } = item;
+  const { id, title, imageUrl, tags, dueDate, assignee } = item;
   return (
     <>
       <Draggable draggableId={`${id}`} index={index}>
@@ -43,7 +46,32 @@ function Card({ item, index, columnTitle }: Props) {
                 />
               </div>
             )}
-            <div className={styles.title}>{title}</div>
+            <div className={styles.cardInfo}>
+              <h3 className={styles.title}>{title}</h3>
+              <div className={styles.contentContainer}>
+                <div className={styles.tagContainer}>
+                  {tags.map((tag, index) => (
+                    <Tag key={index} name={tag} />
+                  ))}
+                </div>
+                <div className={styles.optionalInfo}>
+                  <div className={styles.dueDateWrapper}>
+                    {dueDate && (
+                      <>
+                        <CalendarIcon className={styles.icon} />
+                        <span className={styles.dueDate}>{dueDate}</span>
+                      </>
+                    )}
+                  </div>
+                  {assignee && (
+                    <Avatar
+                      name={assignee.nickname}
+                      className={styles.avatar}
+                    />
+                  )}
+                </div>
+              </div>
+            </div>
           </div>
         )}
       </Draggable>
