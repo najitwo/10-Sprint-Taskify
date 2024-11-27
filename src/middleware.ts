7759adb97ec1c86664e037f41b7fba5442b5ffa9
heejin <dlwlrgkwk82@gmail.com>
2024-11-28 00:13:26 +0900

    ♻️ refactor: modify middleware logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,21 +2,16 @@ import { NextRequest, NextResponse } from 'next/server';
 import { TOKEN_KEY } from './constants/cookies';
 
 export function middleware(request: NextRequest) {
-  const user = request.cookies.get(TOKEN_KEY);
+  const userToken = request.cookies.get(TOKEN_KEY);
 
-  const restrictedPaths = ['/dashboard', '/mypage', '/mydashboard'];
-
-  if (
-    restrictedPaths.some((path) => request.nextUrl.pathname.startsWith(path))
-  ) {
-    if (!user) {
-      return NextResponse.redirect(new URL('/login', request.url));
-    }
+  if (!userToken) {
+    return NextResponse.redirect(new URL('/login', request.url));
   }
 
   return NextResponse.next();
 }
 
+// 아래에서 시작하는 경로에서만 미들웨어 제한
 export const config = {
   matcher: [
     '/dashboard/:path*',
