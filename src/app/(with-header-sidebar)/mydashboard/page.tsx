3c61f2d603d440ee14a6e1bd42a2948da7012328
heejin <77238424+devmanta@users.noreply.github.com>
2024-11-28 10:51:08 +0900

    [style] layout css (#103)
    * 💄 style: main container overflow and background
    
    * 💄 style: expand sidebar add dashboard button area
    
    * ♻️ refactor: extract invitation button component on header
    
    * ✨ feat: clear dashboard store if needed
    
    * 🍱 chore: add og

--- src/app/(with-header-sidebar)/mydashboard/page.tsx
+++ src/app/(with-header-sidebar)/mydashboard/page.tsx
@@ -1,8 +1,18 @@
+'use client';
+
+import { useEffect } from 'react';
+import useDashboardStore from '@/store/dashboardStore';
 import Dashboards from './_components/dashboards/Dashboards';
 import MyInvitations from './_components/invitations/MyInvitations';
 import styles from './page.module.css';
 
 export default function Page() {
+  const { setDashboard } = useDashboardStore();
+
+  useEffect(() => {
+    setDashboard(null);
+  }, []);
+
   return (
     <div className={styles.mydashboard}>
       <Dashboards />
--- src/app/(with-header-sidebar)/mypage/page.tsx
+++ src/app/(with-header-sidebar)/mypage/page.tsx
@@ -1,10 +1,18 @@
 'use client';
 
+import { useEffect } from 'react';
+import useDashboardStore from '@/store/dashboardStore';
 import PasswordForm from './_components/PasswordForm';
 import ProfileForm from './_components/ProfileForm';
 import styles from './page.module.css';
 
 export default function MyPage() {
+  const { setDashboard } = useDashboardStore();
+
+  useEffect(() => {
+    setDashboard(null);
+  }, []);
+
   return (
     <div className={styles.page}>
       <ProfileForm />
