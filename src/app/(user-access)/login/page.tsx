58911d7f0cfe7af79e79fbd780034c6f580cc8be
나승엽 <a01031928263@gmail.com>
2024-11-29 17:36:53 +0900

    Revert ":bug: fix: install js-cookie"
    This reverts commit bcc9109d86a893b5309504c765b60585ee805656.

--- src/app/(user-access)/login/page.tsx
+++ src/app/(user-access)/login/page.tsx
@@ -6,13 +6,11 @@ import { useRouter } from 'next/navigation';
 import useAuthStore from '@/store/authStore';
 import Button from '@/components/Button';
 import { ERROR_MESSAGES } from '@/constants/message';
+import type { User } from '@/types/user';
+import styles from './loginPage.module.css';
 import ModalContainer from '../components/modal/ModalContainer';
 import useModalStore from '../modalStore/modalStore';
-import type { User } from '@/types/user';
-import Cookies from 'js-cookie';
-import { TOKEN_KEY, TOKEN_OPTIONS } from '@/constants/cookies';
-import styles from './loginPage.module.css';
-import axios from 'axios';
+import axios, { AxiosError, isAxiosError } from 'axios';
 
 type LoginFormInputs = {
   email: string;
