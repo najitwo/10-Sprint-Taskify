6c116d0befcfd2f79e5a0ba013473e2e3b0ca434
21ow <kimjinewn@gmail.com>
2024-12-01 20:18:10 +0900

    :lipstick: style: improve UI/UX

--- src/components/landing/Section/PrimarySection.tsx
+++ src/components/landing/Section/PrimarySection.tsx
@@ -1,15 +1,20 @@
 import MainCard from '@/components/landing/Section/MainCard';
 import styles from './PrimarySection.module.css';
 
-export default function PrimarySection() {
+interface Props {
+  inView: boolean;
+}
+
+export default function PrimarySection({ inView }: Props) {
   return (
-    <section className={styles.section}>
+    <section id="secondary" className={styles.section}>
       <MainCard
         title="일의 우선순위를 관리하세요"
         subTitle="Point 1"
         src="/images/landing_lg_1.png"
         alt="우선순위 설정 기능 소개"
         className={styles.cardLg1}
+        inView={inView}
       />
       <MainCard
         title="해야 할 일을 등록하세요"
@@ -18,6 +23,7 @@ export default function PrimarySection() {
         alt="할 일 카드 생성 기능 소개"
         className={styles.cardLg2}
         imgFirst={true}
+        inView={inView}
       />
     </section>
   );
